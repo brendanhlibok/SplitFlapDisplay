c2224e029fce8c00c61d8c8e1934af9f50a8a53b
brendanhlibok <brendanhlibok@gmail.com>
2026-08-13 20:33:38 -0500

    Created control project folder

---
+++ MotorControl/src/main.cpp
@@ -0,0 +1,19 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO GPIO_NUM_2
+
+extern "C" void app_main(void)
+    {
+        gpio_reset_pin(BLINK_GPIO);
+        gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+        while (1) {
+            gpio_set_level(BLINK_GPIO, 1);
+            vTaskDelay(pdMS_TO_TICKS(500));
+            gpio_set_level(BLINK_GPIO, 0);
+            vTaskDelay(pdMS_TO_TICKS(500));
+        }
+
+    }
